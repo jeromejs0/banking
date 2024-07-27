8adb5d4962747e805e453b2bd4acea7cf063368d
jeromejs0 <jerome1610319@gmail.com>
2024-07-27 12:53:10 +0530

    implement initial file and folder structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+export default function RootLayout({  // layout for home dashboard transactions
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <main>
+          SIDEBAR
+          {children}
+      </main>
+    );
+  }
+  
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+export default function RootLayout({  // layout for home dashboard transactions
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        SIDEBAR
+        {children}
+    </main>
+  );
+}
